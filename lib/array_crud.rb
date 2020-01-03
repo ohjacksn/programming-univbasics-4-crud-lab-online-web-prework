6bc2b989081f5bf9c0c74f48048cbf31585c631f
Jackson Gray Morgan <jacksonmo@me.com>
2020-01-03 19:48:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -42,5 +42,6 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-
+  update_element_from_index = ["Lauren", "Jackson", "Sneeze", "Papa"]
+  update_element_from_index
 end
